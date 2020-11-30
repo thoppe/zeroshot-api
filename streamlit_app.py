f9ea2ce25a063ab4ef14640b6de7a01577e39f98
Travis Hoppe <travis.hoppe@gmail.com>
2020-11-30 14:32:22 -0500

    Building out app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import requests
 import pandas as pd
+import json
+from configparser import ConfigParser
 
-zs_url = "http://127.0.0.1:8000"
+config = ConfigParser()
+config.read('config.ini')
+
+zs_url = config.get('streamlit', 'zs_url')
+n_minibatch = int(config.get('streamlit', 'n_minibatch'))
 
 st.title("Zero-shot API with caching")
 
@@ -26,6 +32,11 @@ model_sequences = st.text_area(
 )
 
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+        
 def extract_valid_textlines(block):
     return [x.strip() for x in block.split("\n") if x.strip()]
 
@@ -35,11 +46,33 @@ def get_api_info():
     return r.json()
 
 
-def infer(labes, sequences):
+def infer(labels, sequences):
     params = {"hypothesis": model_hypothesis, "labels": labels, "sequences": sequences}
 
-    r = requests.get(zs_url + "/infer", json=params)
-    df = pd.read_json(r.json())
+    results = {}
+    progress_bar = st.progress(0)
+    n_total_items = len(labels)*len(sequences)
+    n_current = 0
+
+    for label in labels:
+        results[label] = {}
+        
+        for chunk in chunks(sequences, n_minibatch):
+            params = {
+                "hypothesis": model_hypothesis,
+                "labels": [label],
+                "sequences": chunk,
+            }
+            r = requests.get(zs_url + "/infer", json=params)
+            r = json.loads(r.json())
+
+            results[label].update(r[label])
+            n_current += len(chunk)
+
+            progress_bar.progress(n_current/n_total_items)
+            
+    progress_bar.empty()
+    df = pd.DataFrame(results)
     return df
 
 
@@ -52,14 +85,25 @@ info = get_api_info()
 st.sidebar.markdown(f"model name: {info['model_name']}")
 st.sidebar.markdown(f"device: {info['device']}")
 st.sidebar.markdown(f"cached inferences: {info['cached_items']}")
+
+f_dataset = st.sidebar.file_uploader("Upload a CSV")
+if f_dataset is not None:
+    # Example of loading a small dataset
+    df = pd.read_csv(f_dataset)
+    df = df[['text']][:20]
+    sequences = df['text'].tolist()
+else:
+    sequences = extract_valid_textlines(model_sequences)
+
 btn_reset = st.sidebar.button("Reset cache 💣")
 
 if btn_reset:
     reset_cache()
-    info = get_api_info()
 
 labels = extract_valid_textlines(model_labels)
-sequences = extract_valid_textlines(model_sequences)
 
 results = infer(labels, sequences)
-st.table(results)
+
+tableviz = results.style.background_gradient(
+    cmap="Blues",).format("{:0.3f}")
+st.table(tableviz)
